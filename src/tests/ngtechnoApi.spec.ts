f4a2ddcf91af89867c1c7e192b4c4bebc913c6e2
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-04-27 13:10:13 -0300

    refactor: move sut creation to a factory method

--- src/tests/ngtechnoApi.spec.ts
+++ src/tests/ngtechnoApi.spec.ts
@@ -1,15 +1,21 @@
 import axios from "axios";
 import NgtechnoApi from "../utils/api/ngtechnoApi";
 
+const makeSut = (): NgtechnoApi => {
+  return new NgtechnoApi();
+};
+
+const makeFakeParams = () => ({
+  url: "/any-url-test",
+  method: "POST",
+  body: { test: "any-body-test" },
+});
+
 describe("NgTechNo Api Service", () => {
   test("should call axios with correct params when is provided", () => {
-    const sut = new NgtechnoApi();
+    const sut = makeSut();
     const axiosSpy = jest.spyOn(axios, "request");
-    const fakeRequestParams = {
-      url: "/any-url-test",
-      method: "POST",
-      body: { test: "any-body-test" },
-    };
+    const fakeRequestParams = makeFakeParams();
     sut.request(fakeRequestParams);
     expect(axiosSpy).toHaveBeenCalledWith({
       url: `https://cloudng.azurewebsites.net/api${fakeRequestParams.url}`,
