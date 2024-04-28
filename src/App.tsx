7ac5e2689f4be777ef26ea72b8b6739d95697c18
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-04-28 13:53:29 -0300

    refactor: remove unecessary useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
-import { useEffect } from "react";
-import NgtechnoApi from "utils/api/ngtechnoApi";
 
 function App() {
-  useEffect(() => {
-    const ngtechnoApi = new NgtechnoApi();
-    ngtechnoApi.request({
-      url: "/ObterTempoCorredor",
-      body: { idCorrida: 1, numeroCorredor: 5 },
-    });
-  }, []);
-
   return <RouterProvider router={router} />;
 }
 
