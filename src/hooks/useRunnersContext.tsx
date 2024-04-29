6273458aebb48f7483f9e46199b4ffd629f4b37e
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-04-29 19:31:38 -0300

    refactor: change useRunnersContext to a hooks folder

---
+++ src/hooks/useRunnersContext.tsx
@@ -0,0 +1,6 @@
+import { RunnersContext } from "context/runnerContext";
+import { useContext } from "react";
+
+const useRunnersContext = () => useContext(RunnersContext);
+
+export default useRunnersContext;
