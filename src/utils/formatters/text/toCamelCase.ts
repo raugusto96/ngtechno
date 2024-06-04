3705626711922400234c173131a44709c0eba199
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-06-04 10:33:30 -0300

    fix: adjust toCamelCase for special chars

--- src/utils/formatters/text/toCamelCase.ts
+++ src/utils/formatters/text/toCamelCase.ts
@@ -1,5 +1,13 @@
+const normalizeString = (str: string): string => {
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "");
+};
+
 export const toCamelCase = (str: string): string => {
-  return str.replace(
+  const normalizedStr = normalizeString(str);
+  return normalizedStr.replace(
     /(?:^\w|[A-Z]|\b\w|\s+|_+|[^a-zA-Z0-9]+)/g,
     (match, index) => {
       if (/[_\s]/.test(match)) return ""; // Remove underscores and spaces
